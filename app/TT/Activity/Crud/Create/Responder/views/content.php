fed4be5b8fdbd3633cb3c5ce0f89a822addb2eeb
neeru <nbansal@acecharter.org>
2015-12-02 17:37:23 -0500

    activity html

--- app/TT/Activity/Crud/Create/Responder/views/content.php
+++ app/TT/Activity/Crud/Create/Responder/views/content.php
@@ -3,4 +3,16 @@ $data = $this->data;
 
 echo 
 $helper->tag('main') .
+	$helper->tag('section',['class'=>'home-section text-center']) .
+	$helper->tag('div',['class'=>'container wow bounceInDown','data-wow-delay'=>'2s']) .
+		$helper->tag('div',['class'=>'row']) . $helper->tag('h2') . $h('Create Activity') . $helper->tag('/h2') . $helper->tag('/div') .
+		$helper->tag('div',['class'=>'col-xs-6 col-xs-offset-3 panel panel-admin']) .
+			$helper->tag('div',['class'=>'row']) . $helper->label($data['title_label']['val'])->before($helper->input($data['title_input'])) . $helper->tag('/div') .
+			$helper->tag('div',['class'=>'row']) . $helper->label($data['activity_label']['val'])->before($helper->input($data['activity_input'])) . $helper->tag('/div') .	
+			$helper->tag('div',['class'=>'row']) . $helper->label($data['description_label']['val'])->before($helper->input($data['description_input'])) . $helper->tag('/div') .		
+			$helper->tag('div',['class'=>'row']) . $helper->label($data['time_label']['val'])->before($helper->input($data['time_input'])) . $helper->tag('/div') .
+			$helper->tag('div',['class'=>'row']) . $helper->input($data['create_btn']) . $helper->tag('/div') .
+		$helper->tag('/div') .			
+	$helper->tag('/div') .
+	$helper->tag('/section') .
 $helper->tag('/main') ;
