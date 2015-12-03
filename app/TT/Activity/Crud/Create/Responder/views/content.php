16a4286b76d010e34ba6f2d6b265fc9426505db4
neeru <nbansal@acecharter.org>
2015-12-02 20:57:51 -0500

    update activity html

--- app/TT/Activity/Crud/Create/Responder/views/content.php
+++ app/TT/Activity/Crud/Create/Responder/views/content.php
@@ -7,11 +7,26 @@ $helper->tag('main') .
 	$helper->tag('div',['class'=>'container wow bounceInDown','data-wow-delay'=>'2s']) .
 		$helper->tag('div',['class'=>'row']) . $helper->tag('h2') . $h('Create Activity') . $helper->tag('/h2') . $helper->tag('/div') .
 		$helper->tag('div',['class'=>'col-xs-6 col-xs-offset-3 panel panel-admin']) .
-			$helper->tag('div',['class'=>'row']) . $helper->label($data['title_label']['val'])->before($helper->input($data['title_input'])) . $helper->tag('/div') .
-			$helper->tag('div',['class'=>'row']) . $helper->label($data['activity_label']['val'])->before($helper->input($data['activity_input'])) . $helper->tag('/div') .	
-			$helper->tag('div',['class'=>'row']) . $helper->label($data['description_label']['val'])->before($helper->input($data['description_input'])) . $helper->tag('/div') .		
-			$helper->tag('div',['class'=>'row']) . $helper->label($data['time_label']['val'])->before($helper->input($data['time_input'])) . $helper->tag('/div') .
+		$helper->form(array('method'=>'post','action'=>'/activity','accept-charset'=>'UTF-8','enctype'=>'multipart/form-data')) .			
+			$helper->tag('div',['class'=>'form-group']) .
+				$helper->label($data['title_label']['val']) .
+				$helper->input($data['title_input']). 
+			$helper->tag('/div') .					
+			$helper->tag('div',['class'=>'form-group']) . 
+				$helper->label($data['activity_label']['val']) .
+				$helper->input($data['activity_input']).
+			$helper->tag('/div') .
+			$helper->tag('div',['class'=>'form-group']) .
+				$helper->label($data['description_label']['val']) .
+				$helper->input($data['description_input']) .
+			$helper->tag('/div') .		
+			$helper->tag('div',['class'=>'form-group']) .
+				$helper->label($data['time_label']['val']) .
+				$helper->input($data['time_input']) .
+				$helper->tag('/div') .
+			$helper->input($data['hidden_input']) .
 			$helper->tag('div',['class'=>'row']) . $helper->input($data['create_btn']) . $helper->tag('/div') .
+		$helper->tag('/form') .
 		$helper->tag('/div') .			
 	$helper->tag('/div') .
 	$helper->tag('/section') .
