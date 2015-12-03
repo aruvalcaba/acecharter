a996d6887cac4332ebb621a73e0a586765d80a03
neeru <nbansal@acecharter.org>
2015-12-02 22:51:25 -0500

    alert file

--- app/views/base/alerts.php
+++ app/views/base/alerts.php
@@ -4,7 +4,8 @@ $alert_class = $alerts['class'];
 $alert_messages = $alerts['messages'] ;
 
 echo 
-$helper->tag('div',$alert_class) .
+$helper->tag('div',['class'=> $alert_class]) .
+>>>>>>> b36600a... alert file
 	$helper->a('#','&times', array('class'=>'close','data-dismiss'=>'alert')) .
 	$helper->tag('ul') ;
 		foreach($alert_messages as $message){
