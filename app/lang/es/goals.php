345563159ec254c447fd426828f726c91aedd8a6
neeru <nbansal@acecharter.org>
2016-01-07 20:14:54 -0500

    add goal username

--- app/lang/es/goals.php
+++ app/lang/es/goals.php
@@ -1,43 +1,46 @@
 <?php
 
 return [
-        'goal_1_detail' => 'It is important that :name attend school regularly. Students that miss more than 10% of the school year, are at-risk for dropping out of high school and not graduating on time.',
-		'goal_1_detail_2' => 'Here are five things you can do at home to help :name make it school on-time, every time.
-							<ul>
-								<li>First thing</li>
-								<li>Second thing</li>
-								<li>Third thing</li>
-								<li>Fourth thing</li>
-								<li>Fifth thing</li>
-							</ul>',
-		'goal_2_detail' => 'At ACE, student behavior is tracked using Kickboard.  We expect our students to earn merits and avoid de-merits.',
-		'goal_2_detail_2' => 'Here are five things you can do at home to help :name make it school on-time, every time.
-							<ul>
-								<li>First thing</li>
-								<li>Second thing</li>
-								<li>Third thing</li>
-								<li>Fourth thing</li>
-								<li>Fifth thing</li>
-							</ul>',
+		'goal_1_positive' => ':name is completing their homework regularly. Great job!',
+		'goal_1_nagative' => ':name is not completing their homework regularly. Students that finish homework are more likely to succeed academically. Click “more info” below to learn what you can do at home to help.',
+        'goal_1_detail' => ':name should finish their daily homework. This will help them be ready for the next day’s lesson and be able to work independently. Here are some strategies to help your child reach their homework goal:
+<ul><li>Login to ACE Family Link every Thursday to review your child’s homework completion. If they finished their homework everyday for the week, give them a small reward (a visit to the park, a new book, a break from doing chores, a special treat).</li>
+<li>
+The most frequent reason students give for not doing their homework is that “they forgot.” Ask your child every night if they finished their homework. Set a regular time for homework. Students should spend at least an hour on homework every night. Remove distractions by turning off the TV and other screens until homework is complete.</li>
+<li>Talk to your child about the homework assignments. Ask them: Do you understand what you’re supposed to do? Do you need help in understanding how to do this assignment? Do you have everything you need to do the assignment? If your child has questions, you may want to call or meet with the teacher.</li></ul>',
+		'goal_1_detail_2' => 'Material adapted from <a target="_blank" href="http://www2.ed.gov/parents/academic/help/homework/homework.pdf">Department of Education</a>',
+		'goal_2_positive' => ':name is attending school regularly. Great job!',
+		'goal_2_negative' => ':name is not attending school regularly. Students that miss school are at-risk for dropping out of school and not graduating on time. Click “more info” below to learn what you can do at home to help.',
+		'goal_2_detail' => ':name should attend school regularly. Research shows that students that miss school even two or three days a month - excused or unexcused - are at risk of falling behind. Here are some strategies to help your child reach their attendance goal:
+<ul><li>
+Login to ACE Family Link every Thursday to review your child’s attendance. If they attended school everyday for the week, give them a small reward (a visit to the park, a new book, a break from doing chores, a special treat).</li>
+<li>Set and enforce a consistent bedtime so that your child is fully rested the next day. The Center for Disease Control and Prevention recommends at least ten hours of sleep for school-aged children.</li>
+<li>If your child has a cold but no fever (less than 100 degrees), send them to school. If you don’t have a thermometer, please purchase one.
+You should set weekday medical and dental appointments only during after-school hours. If you are planning a vacation, check with the school calendar to make sure your child will not miss any school.</li>
+<li>Find a relative, friend, or neighbor who can  take your child to school if you cannot make it or if your child has missed the bus. If transportation is a problem, you can also ask the school principal for help.</li>
+<li>Your child may be missing school in order to avoid bullying. Talk to your child and the principal if necessary.</li></ul>',
+		'goal_2_detail_2' => 'Learn more by watching these videos: <a target="_blank" href="https://vimeo.com/101425388">https://vimeo.com/101425388</a> <br><br>Material adapted from <a target="_blank" href="http://attendanceworks.org">attendanceworks.org</a>',
+		'goal_3_positive' => ':name has positive behavior at school meaning no detentions or suspensions. Great job!',
+		'goal_3_negative' => ':name does not have positive behavior at school. Students that misbehave are at-risk for falling behind academically. Click “more info” below to learn what you can do at home to help.
+',
+		'goal_3_detail' => ':name should make positive choices at school. This means they should not receive detentions or suspensions. Here are some strategies to help your child reach their behavior goal:
+<ul><li>
+Login to ACE Family Link every Thursday to review your child’s behavior. If they showed positive behavior, give them a small reward (a visit to the park, a new book, a break from doing chores, a special treat).</li>
+<li>If your child is involved in an incident, ask them about what happened. Find out what punishment the principal or teacher said they would impose (detention, suspension, expulsion, etc.). Use this opportunity to find out if your child feels misunderstood or wrongly accused. Also, ask whether there are problems at school that your child was reacting to, like being bullied or having trouble keeping up in class.</li> 
+<li>Students with disabilities have additional rights relating to school discipline. If your child is receiving special education services or might be eligible for them, be sure to ask for policies and procedures for the discipline of students with disabilities.</li> 
+',
+		'goal_3_detail_2' => '',
+		'goal_4_positive' =>':name is passing all their classes. Great job!',
+		'goal_4_negative' => ':name is failing one or more of their classes. Click “more info” below to learn what you can do at home to help.',
 
-		'goal_3_detail' => 'It is important that :name attend school regularly. Students that miss more than 10% of the school year, are at-risk for dropping out of high school and not graduating on time.',
-		'goal_3_detail_2' => 'Here are five things you can do at home to help :name make it school on-time, every time.
-							<ul>
-								<li>First thing</li>
-								<li>Second thing</li>
-								<li>Third thing</li>
-								<li>Fourth thing</li>
-								<li>Fifth thing</li>
-							</ul>',
+		'goal_4_detail' => ':name should earn a passing grade in every subject. This means they have successfully learned the skills and knowledge required to move to the next grade. Here are some strategies to help your child reach their academic success goal:
+<ul><li>
+Login to ACE Family Link every Thursday to review your child’s academic record. If they are passing every subject, give them a small reward (a visit to the park, a new book, a break from doing chores, a special treat).</li>
+<li>If your child is not passing a particular subject, call the school to organize a parent teacher conference to discuss the academic record, including missed assignments. Some teachers can provide extra work for students so they can earn additional points in order to pass a class.
+Talk to your child about the importance of having a growth mindset. Links to PDF (download and store in internal server) <a target="_blank" href="http://bms.rfsd.k12.co.us/documents/MindsetIntroForParents-English.pdf"> http://bms.rfsd.k12.co.us/documents/MindsetIntroForParents-English.pdf </a></li>
+</ul>
+',
+		'goal_4_detail_2' => '',
 
-		'goal_4_detail' => 'It is important that :name attend school regularly. Students that miss more than 10% of the school year, are at-risk for dropping out of high school and not graduating on time.',
-		'goal_4_detail_2' => 'Here are five things you can do at home to help :name make it school on-time, every time.
-							<ul>
-								<li>First thing</li>
-								<li>Second thing</li>
-								<li>Third thing</li>
-								<li>Fourth thing</li>
-								<li>Fifth thing</li>
-							</ul>',
-
+		
 ];
