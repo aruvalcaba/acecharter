ebb7e3a98f477dec8c4016170d8172f46225db91
Alan Ruvalcaba <aruval3@gmail.com>
2016-01-12 07:08:20 -0500

    Update admin home to show teacher classroom data

--- app/TT/Home/Admin/Responder/views/content.php
+++ app/TT/Home/Admin/Responder/views/content.php
@@ -14,7 +14,12 @@ include (sprintf('%s/views/base/%s',app_path(),'alerts.php'));
 
 echo
 $helper->tag('div',['class'=>'table-responsive','style'=>'margin-top: 15px']) .
-	$helper->tag('table',['class'=>'table table-striped table-bordered','style'=>'margin-top: 15px']) .
+    $helper->tag('table',['class'=>'table table-striped table-bordered','style'=>'margin-top: 15px']) .
+        $helper->tag('tr').
+            $helper->tag('td') . 'Teacher' . $helper->tag('/td') .
+            $helper->tag('td') . 'Number of students in classroom' . $helper->tag('/td') .
+        $helper->tag('/tr');
+/**
 		$helper->tag('tr') .
 			$helper->tag('td') . 'Title' . $helper->tag('/td'). 
 			$helper->tag('td') . 'Avg. Rating' . $helper->tag('/td').
@@ -22,19 +27,14 @@ $helper->tag('div',['class'=>'table-responsive','style'=>'margin-top: 15px']) .
 			$helper->tag('td') . '% Appropriate for Q2' . $helper->tag('/td').
 			$helper->tag('td') . '% Parents who finished this activity' . $helper->tag('/td').
 			$helper->tag('td') . 'Actions' . $helper->tag('/td');
-		$helper->tag('/tr') ;
-if(count($this->activities) > 0 ) {
-    foreach($this->activities as $activity) {
-		$rating = $activity->avgRating();	
-		$deleteString = sprintf("'%s','%s','%s'",$activity->id,'activity',$activity->title);
-        echo $helper->tag('tr',['id'=>$activity->title]) .
-				$helper->tag('td') . $activity->title . $helper->tag('/td') .
-				$helper->tag('td') . $rating . $helper->tag('/td') .	
-        		$helper->tag('td') . $activity->q1Percent() . $helper->tag('/td') .
-				$helper->tag('td') . $activity->q2Percent() . $helper->tag('/td') .
-				$helper->tag('td') . $activity->finishedPercent() . $helper->tag('/td') .
-				$helper->tag('td') . $helper->a(URL::route('activity.edit',[$activity->id]), 'Edit' , array('class'=>'btn btn-warning')) . $helper->a('#','Delete',array('data-token'=>csrf_token(),'class'=>'btn btn-danger','style'=>'margin-left: 15px', 'onclick'=>'destroy('.$deleteString.')')) . $helper->tag('/td') .
-			$helper->tag('/tr') ;
+$helper->tag('/tr') ;
+**/
+if(count($this->teacherData) > 0 ) {
+    foreach($this->teacherData as $row) {
+        echo $helper->tag('tr') .
+				$helper->tag('td') . $row['teacherLastName'] . $helper->tag('/td') .
+				$helper->tag('td') . $row['studentCount'] . $helper->tag('/td') .	
+			 $helper->tag('/tr') ;
     }
 }
 echo
