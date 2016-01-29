e3549ea320fc2b8f8f1e131325af4a3d783136bf
Alan Ruvalcaba <aruval3@gmail.com>
2016-01-29 11:37:45 -0500

    Delete unecessary files

--- app/TT/Goal/Responder/views/goal_4/goal.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include ('head.php'); 
-    include (sprintf('%s/views/base/%s',app_path(),'nav.php'));
-    include ('content.php');
-	include (sprintf('%s/views/base/%s',app_path(),'footer.php'));
-    include ('scripts.php');
-?>
-
