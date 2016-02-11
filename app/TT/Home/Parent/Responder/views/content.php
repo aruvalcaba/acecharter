f2bc9ef7f502d4772dd92fac9fbdee5880d95488
dev <dev@sepultura.sepultura>
2016-02-11 16:14:51 -0500

    add link in welcome email temlate

--- app/TT/Home/Parent/Responder/views/content.php
+++ app/TT/Home/Parent/Responder/views/content.php
@@ -6,7 +6,7 @@ $helper->tag('section',['id'=>'service','class'=>'home-section text-center']) .
 	$helper->tag('div',['class'=>'container']) .
 		$helper->tag('div',['class'=>'heading-about']) .
 			$helper->tag('div',['class'=>'row col-lg-6 col-lg-offset-3 col-sm-8 col-sm-offset-2']) .
-				$helper->tag('h2') . $data['welcome']['val'] .'&nbsp;' . '&nbsp;' . $h($this->student->first_name) . $helper->tag('/h2') .
+				$helper->tag('h2') . $data['welcome']['val'] .'&nbsp;' . '&nbsp;' . $h($this->user->first_name) . $helper->tag('/h2') .
 				$helper->tag('i',['class'=>'fa fa-2x fa-angle-down']) . $helper->tag('/i') .
 			$helper->tag('/div') .
       	$helper->tag('/div') ;
